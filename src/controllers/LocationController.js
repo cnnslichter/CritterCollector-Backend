1454497a18fdc016ae08dc2160aabe1d8af1cf93
tyler.allen <tyler.allen@ufl.edu>
2021-10-10 21:22:51 -0400

    Create placeholder controllers for location and animal

---
+++ src/controllers/LocationController.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+const config = require('../config.json')
+const dotenv = require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient
+const client = MongoClient(process.env.DB_URI || config["DB_URI"], { useNewUrlParser: true })
+
+exports.checkLocation = async (req, res) => {
+
+}
