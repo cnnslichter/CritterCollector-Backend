8dbec5677ae1f931e9a6e9f08b2b28d899b088a8
tyler.allen <tyler.allen@ufl.edu>
2021-10-10 21:30:20 -0400

    Move code for MoL API call into SpawnController

--- src/controllers/SpawnController.js
+++ src/controllers/SpawnController.js
@@ -1,8 +1,9 @@
 const assert = require('assert')
+const axios = require('axios')
 const config = require('../config.json')
 const dotenv = require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient
-const animalDataController = require('./AnimalDataController')
+const wikiInfoCollector = require('./WikipediaInfoCollector')
 const client = MongoClient(process.env.DB_URI || config["DB_URI"], { useNewUrlParser: true })
 
 exports.findSpawner = async (req, res) => {
@@ -28,7 +29,7 @@ exports.createSpawner = async (req, res) => {
     await client.connect()
 
     try {
-        let data = await animalDataController.getAnimalData(req.query.lat, req.query.long)
+        let data = await getAnimalData(req.query.lat, req.query.long)
         const newSpawn = {
             "createdAt": new Date(),    //used for expiring docs 
             "coordinates": coords,
@@ -66,6 +67,66 @@ findNearestSpawns = async (maxDist /*in meters*/, coords) => {
         // console.log(e)
         throw e
     }
+}
+
+getAnimalData = async (lat, long) => {
+    const queryUrl =
+        'https://api.mol.org/1.x/spatial/species/list?callback=angular.callbacks._2w&lang=en' +
+        '&lat=' + lat +
+        '&lng=' + long +
+        '&radius=' + (process.env.ANIMAL_SEARCH_RADIUS || config['ANIMAL_SEARCH_RADIUS'])
+
+    let result = await axios(queryUrl)
+    var data = cleanData(result['data'])
+    var parsedData = JSON.parse(data)
+
+    if (dataIsValid(parsedData)) {
+        var listOfAllAnimals = getAllAnimals(parsedData)
+        const shortList = listOfAllAnimals.slice(0, 10)
+        infolist = await wikiInfoCollector.getAnimalsWiki(shortList)
+        return infolist
+    }
+    else {
+        throw Exception()
+    }
+}
+
+/**
+ * The MOL API returns some angular callback prefix in front of
+ * all json, so this is a simple substring operation to remove it.
+ */
+function cleanData(data) {
+    data = data.substring(22)
+    data = data.substring(0, data.length - 1)
+    return data
+}
+
+/**
+ * It seems like the MOL API only provides the "error" key if there is an error,
+ * otherwise it will not be present
+ */
+function dataIsValid(data) {
+    // they pass within an array for some reason, so we must use index 0
+    return !('error' in data[0])
+}
+
+function getAllAnimals(data) {
+    var listOfAllAnimals = []
+
+    for (var animalTypeKey in data) {
+        if (!config.EXCLUDED_TYPES.includes(data[animalTypeKey]['taxa'])) {
+            var listOfSpecies = data[animalTypeKey]['species']
+
+            for (var speciesKey in listOfSpecies) {
+                listOfAllAnimals.push({
+                    "Scientific_Name": listOfSpecies[speciesKey]['scientificname'],
+                    "Common_Name": listOfSpecies[speciesKey]['common']
+                })
+            }
+        }
+    }
+
+    return listOfAllAnimals
 }
 
 insertNewSpawn = async (document) => {
