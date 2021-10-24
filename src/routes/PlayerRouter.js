091e8bb41724a138fd54ecd4b8e1d4564f5ec1e7
nmoody9899 <63110489+nmoody9899@users.noreply.github.com>
2021-10-24 13:52:22 -0700

    Commented routes for player controller

--- src/routes/PlayerRouter.js
+++ src/routes/PlayerRouter.js
@@ -3,9 +3,9 @@ const playerController = require('../controllers/PlayerController')
 
 let router = express.Router();
 
-router.get('/', playerController.checkProfile);
-router.post('/', playerController.createNewProfile);
-router.put('/', playerController.updateProfile);
-router.delete('/', playerController.deleteProfile);
+//router.get('/', playerController.checkProfile);
+//router.post('/', playerController.createNewProfile);
+//router.put('/', playerController.updateProfile);
+//router.delete('/', playerController.deleteProfile);
 
 module.exports = router;
