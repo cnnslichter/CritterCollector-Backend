8979603b19cdf9afd0bf2f96c10876301acbb762
nmoody9899 <63110489+nmoody9899@users.noreply.github.com>
2021-10-26 22:13:14 -0700

    Added functionality to add/remove animal from special location and delete a special location

--- src/controllers/LocationController.js
+++ src/controllers/LocationController.js
@@ -70,7 +70,117 @@ addSpecialLocation = async (location, coordinates) => {
 //addSpecialLocation('UF CISE Building', [[29.649661, -82.344315], [29.649642, -82.343570], [29.648682, -82.343588], [29.648689, -82.344771], [29.649661, -82.344315]])
 
 // Add animal to special spawn location
+addSpecialAnimal = async (location, scientific_animal, common_animal) => {
+    await client.connect();
+
+    try {
+        const database = client.db('Animal-Game');
+
+        // Checks if an animal already exists at the special location
+        let animal_exists = await database.collection('Special-Locations').find({
+            name: location,
+            animals: { $elemMatch: {
+                scientific_name: scientific_animal,
+                common_name: common_animal
+            } }
+        }).toArray();
+
+        if (animal_exists.length > 0) {
+            // Returned record indicates animal already exists for location
+            await client.close();
+            return "Animal was already added.";
+        }
+        else {
+            // Updates the location record with the new animal
+            const query = {
+                name: location
+            };
+
+            const newAnimal = {
+                $push: {
+                    animals: {
+                        scientific_name: scientific_animal,
+                        common_name: common_animal
+                    }
+                }
+            };
+
+            await database.collection('Special-Locations').updateOne(query, newAnimal);
+        }
+
+        await client.close();
+        return "Animal added.";
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+//addSpecialAnimal('UF CISE Building', 'Uffes Professorus', 'UF Professor');
 
 // Remove animal from special spawn location
+removeSpecialAnimal = async(location, scientific_animal) => {
+    await client.connect();
+
+    try {
+        const database = client.db('Animal-Game');
+
+        // Checks if animal already exists at special location
+        let animal_exists = await database.collection('Special-Locations').find({
+            name: location,
+            animals: { $elemMatch: {
+                scientific_name: scientific_animal
+            } }
+        }).toArray(); 
+
+        if (animal_exists.length > 0) {
+            // Removes the animal at the special location
+            const query = {
+                name: location
+            };
+
+            const delAnimal = {
+                $pull: {
+                    animals: {
+                        scientific_name: scientific_animal
+                    }
+                }
+            }
+
+            await database.collection('Special-Locations').updateOne(query, delAnimal);
+        }
+        else {
+            console.log("hi");
+            // No animal_exist array indicates animal does not exist
+            await client.close();
+            return "Animal does not exist at special location.";
+        }
+        console.log("hi2");
+
+        await client.close();
+        return "Animal removed.";
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+//removeSpecialAnimal('UF CISE Building', 'Uffes Professorus');
 
 // Remove special spawn location
+removeSpecialLocation = async (location) => {
+    await client.connect();
+
+    try {
+        const query = { name: location };
+
+        await client.db('Animal-Game').collection('Special-Locations').deleteOne(query);
+
+        await client.close();
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+//removeSpecialLocation('Test Delete');
