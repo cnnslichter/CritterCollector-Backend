d4284b1674ef0d07dac97f2efcb59416d2790d6c
tyler.allen <tyler.allen@ufl.edu>
2021-11-08 08:15:33 -0500

    Update server file to use single connection and shared database throughout application.

--- src/server.js
+++ src/server.js
@@ -1,17 +1,23 @@
 const config = require('./config.json');
 const dotenv = require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
-const client = MongoClient(process.env.DB_URI || config["DB_URI"], { useNewUrlParser: true });
+const mongoURI = process.env.DB_URI || config["DB_URI"];
 
 const createServer = require('./app.js');
 
 try {
-    const app = createServer();
-    app.listen(process.env.PORT || config['PORT']);
-    console.log("Started Animal Location Server!");
+    MongoClient.connect(mongoURI, { useNewUrlParser: true }, (err, db) => {
+        if (err) throw err;
 
+        const app = createServer();
+
+        app.locals.db = db;
+
+        app.listen(process.env.PORT || config['PORT']);
+        console.log("Started Animal Location Server!");
+    });
 } catch (error) {
     console.log(error);
 } finally {
-    client.close();
+    MongoClient.close();
 }
