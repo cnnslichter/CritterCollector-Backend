696d828a21f41be26bcfd986af6748f4323c3d4c
tyler.allen <tyler.allen@ufl.edu>
2021-11-08 08:33:41 -0500

    Remove unused imports from AnimalController

--- src/controllers/AnimalController.js
+++ src/controllers/AnimalController.js
@@ -1,9 +1,3 @@
-const assert = require('assert')
-const config = require('../config.json')
-const dotenv = require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient
-const client = MongoClient(process.env.DB_URI || config["DB_URI"], { useNewUrlParser: true })
-
 /*
 * Adds an animal to the specified special location
 */
