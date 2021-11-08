15e02f9e53e94b35191d0dd9f149a3020e427fb6
tyler.allen <tyler.allen@ufl.edu>
2021-11-08 08:32:51 -0500

    Update LocationController with error checking and extract some logic to DatabaseService

--- src/controllers/LocationController.js
+++ src/controllers/LocationController.js
@@ -1,43 +1,37 @@
-const assert = require('assert')
-const config = require('../config.json')
-const dotenv = require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient
-const client = MongoClient(process.env.DB_URI || config["DB_URI"], { useNewUrlParser: true })
+const DatabaseService = require('../services/DatabaseService');
+const ValidationService = require('../services/ValidationService');
 
 /*
  * Checks if player latitude/longitude is within a "Special Spawn" location
  *
  * To Do:
- *      - Update parameters to fit server request
  *      - Consider necessity of array (can special locations overlap?)
- *      - May want to remove 'await client.close()' when done using locally
  *      - Do we want to integrate $maxDistance so user does not have to be IN the lake/special location?
  */
 exports.checkLocation = async (req, res) => {
 
-    await client.connect();
+    const { longitude, latitude } = req.query;
+
+    if (!longitude || !latitude) {
+        return res.status(400).end();
+    }
 
     try {
-        const database = client.db('Animal-Game');
+        const errors = ValidationService.checkCoordinates(longitude, latitude);
 
-        // Queries Special-Spawn-Points cluster for region that intersects the point, returns only location name
-        console.log("Finding special location...");
-        let special_location = await database.collection('Special-Locations').find({
-            region: {
-                $geoIntersects: {
-                    $geometry: {
-                        type: "Point",
-                        coordinates: [req.query.lat, req.query.long]
-                    }
-                }
-            }
-        }, { projection: { _id: 0, name: 1 } }).toArray();
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors });
+        }
 
-        let location_name = (special_location.length > 0) ? special_location[0].name : "Not Found";
-        console.log(`Special location: ${location_name}`);
-        await client.close();
+        const database = req.app.locals.db;
+        const coordinates = [parseFloat(longitude), parseFloat(latitude)];
 
-        return location_name;
+        let special_location = DatabaseService.findSpecialLocation(database, coordinates);        
+
+        let location_name = (special_location.length > 0) ? special_location[0].name : "Special Location Not Found";
+
+        res.status(200).json({ "special_location": location_name });
+
     } catch (error) {
         console.error(error);
     }
