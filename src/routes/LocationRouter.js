2fef56f198b3a3d429702eb4d25fa64c66814d17
nmoody9899 <63110489+nmoody9899@users.noreply.github.com>
2021-11-11 14:18:42 -0800

    Added placeholders for location routes

--- src/routes/LocationRouter.js
+++ src/routes/LocationRouter.js
@@ -4,5 +4,7 @@ const locationController = require('../controllers/LocationController')
 let router = express.Router();
 
 router.get('/', locationController.checkLocation);
+//router.post('/', locationController.addSpecialLocation);
+//router.delete('/', locationController.deleteSpecialLocation);
 
 module.exports = router;
