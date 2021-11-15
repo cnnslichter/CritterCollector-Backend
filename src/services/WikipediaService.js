6c57f20db9eaff990278778fc529451930283d10
tyler.allen <tyler.allen@ufl.edu>
2021-11-15 13:59:24 -0500

    Extract new functionality from WikipediaInfoCollector into WikipediaService.

--- src/services/WikipediaService.js
+++ src/services/WikipediaService.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 
 /**
- * For each animal found on Wikipedia, creates object with common and scientific names, image link, and description.
+ * For each animal found on Wikipedia, creates object with common and scientific names, raw image, 
+ * image link, and description.
  * If animal is not found on Wikipedia, it is removed from the list.
  */
 exports.getAnimalsWiki = async (selectedAnimals) => {
@@ -14,8 +15,9 @@ exports.getAnimalsWiki = async (selectedAnimals) => {
                 return {
                     "Common_Name": `${Animal["Common_Name"]}`,
                     "Scientific_Name": `${Animal["Scientific_Name"]}`,
-                    "Image_Link": wikiInfo["img"],
-                    "Description": wikiInfo["desc"]
+                    "Raw_Image": wikiInfo.b64image,
+                    "Image_Link": wikiInfo.imglink,
+                    "Description": wikiInfo.desc
                 };
             }
 
@@ -37,6 +39,7 @@ exports.getAnimalsWiki = async (selectedAnimals) => {
  * If the animal is found, returns an object with a description and link to animal's image.
  */
 exports.getInfo = async (AnimalName) => {
+    // adjust pithumbsize if the image resolution is too low
     const queryUrl =
         'https://en.wikipedia.org/w/api.php?action=query&format=json' +
         '&titles=' + encodeURIComponent(AnimalName) +
@@ -45,16 +48,23 @@ exports.getInfo = async (AnimalName) => {
     try {
         let result = await axios(queryUrl);
 
-        const pageid = Object.keys(result['data']["query"]["pages"])[0]; // get the number of the first page in the list
+        const pageid = Object.keys(result.data.query.pages)[0]; // get the number of the first page in the list
 
         if (pageid == -1) {  // checks if the wiki page is valid/exists
             return null;
         } else {
-            var wikiInfo = { 
-                img: result['data']["query"]["pages"][pageid]["thumbnail"]["source"], 
-                desc: result['data']["query"]["pages"][pageid]["extract"]
+            var page = result.data.query.pages[pageid];
+            try {
+                var imageResult = await axios.get(page.thumbnail.source, { responseType: "arraybuffer" }); // query the image url
+                var wikiInfo = {
+                    b64image: "data:" + imageResult.headers["content-type"] + ";base64," + Buffer.from(imageResult.data).toString("base64"), // this is not really space-efficient
+                    imglink: page.thumbnail.source,
+                    desc: page.extract
+                }
+                return wikiInfo;
+            } catch (error) {
+                return null;
             }
-            return wikiInfo;
         }
     } catch (error) {
         console.log("ERROR: at Wikipedia api");
