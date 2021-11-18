3a184675624d0c768d63e1efcd509eedbd69e0cb
tyler.allen <tyler.allen@ufl.edu>
2021-11-18 02:02:48 -0500

    Add player router and JSON bodyparser to app file

--- src/app.js
+++ src/app.js
@@ -2,15 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const animalRouter = require('./routes/AnimalRouter');
 const locationRouter = require('./routes/LocationRouter');
+const playerRouter = require('./routes/PlayerRouter');
 const spawnRouter = require('./routes/SpawnRouter');
 const specialSpawnRouter = require('./routes/SpecialSpawnRouter');
 
 function createServer() {
     const app = express();
-    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
     app.use('/api/animal', animalRouter);
     app.use('/api/location', locationRouter);
+    app.use('/api/player', playerRouter);
     app.use('/api/spawner', spawnRouter);
     app.use('/api/special-spawner', specialSpawnRouter);
 
