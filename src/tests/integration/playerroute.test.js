3df33a40b2e7e3479f436030e3d235632464c09f
matt-iknow <60084825+matt-iknow@users.noreply.github.com>
2022-04-04 20:32:39 -0400

    add new playerroute tests

--- src/tests/integration/playerroute.test.js
+++ src/tests/integration/playerroute.test.js
@@ -1,10 +1,13 @@
 const request = require('supertest');
 const createServer = require('../../app');
 const MongoClient = require('mongodb').MongoClient;
+const axios = require('axios');
 const DatabaseService = require('../../services/DatabaseService');
 
 let db;
 let playerProfiles;
+
+jest.mock('axios');
 
 const app = createServer();
 
@@ -263,6 +266,354 @@ describe('POST - /api/player', () => {
     })
 })
 
+describe('DELETE - /api/player', () => {
+
+    describe('Error Checking', () => {
+
+        beforeEach(async () => {
+            await playerProfiles.deleteMany();
+        });
+
+        it('should return 400 when username parameter is not part of the request', async () => {
+
+            const response = await request(app)
+                                    .delete("/api/player")
+                                    .send({});
+
+            expect(response.status).toBe(400);
+            expect(response.body).toMatchObject({});
+        })
+
+        it('should return 422 when profile is not removed successfully', async () => {
+
+            const username = "NonExistantUser";
+
+            const response = await request(app)
+                                    .delete("/api/player")
+                                    .send({
+                                        username: username
+                                    });
+
+            expect(response.status).toBe(422);
+        })
+
+        it('should indicate the profile was not removed when remove is not successful', async () => {
+
+            const username = "NonExistantUser";
+
+            const response = await request(app)
+                                    .delete("/api/player")
+                                    .send({
+                                        username: username
+                                    });
+
+            const deleteMessage = response.body.remove_profile;
+
+            expect(deleteMessage).toEqual(expect.stringContaining(
+                "Profile not removed successfully"
+            ))
+        })
+
+        it('should return DELETE error in response if any errors are thrown', async () => {
+
+            jest.spyOn(DatabaseService, 'removePlayerProfile').mockRejectedValue();
+
+            const validName = "ValidUsername";
+
+            const response = await request(app)
+                                    .delete("/api/player")
+                                    .send({
+                                        username: validName
+                                    });
+
+            expect(response.error.status).toBe(404);
+
+            expect(response.error.text).toEqual(expect.stringContaining(
+                "Cannot DELETE /api/player"
+            ));
+
+            jest.spyOn(DatabaseService, 'removePlayerProfile').mockRestore();
+        })
+    })
+
+    describe('Valid Call', () => {
+
+        beforeEach(async () => {
+            await playerProfiles.deleteMany();
+
+            const username = "Knight";
+            const email = "random@email.com";
+
+            const newProfile = {
+                "user_name": username,
+                "user_email": email,
+                "collection": [
+                    {
+                        "common_name": "American alligator",
+                        "scientific_name": "Alligator mississippiensis",
+                        "count": 1
+                    }
+                ]
+            }
+
+            await playerProfiles.insertOne(newProfile);
+        });
+
+        it('should return 200 when parameters are valid and player profile is removed', async () => {
+
+            const username = "Knight";
+
+            const response = await request(app)
+                                    .delete("/api/player")
+                                    .send({
+                                        username: username
+                                    });
+
+            expect(response.status).toBe(200);
+        })
+
+        it('should indicate the profile was removed when remove is successful', async () => {
+
+            const username = "Knight";
+
+            const response = await request(app)
+                                    .delete("/api/player")
+                                    .send({
+                                        username: username
+                                    });
+
+            const deleteMessage = response.body.remove_profile;
+
+            expect(deleteMessage).toEqual(expect.stringContaining(
+                "Profile removed successfully"
+            ))
+        })
+    })
+})
+
+describe('GET - /api/player/box', () => {
+
+    describe('Error Checking', () => {
+
+        beforeEach(async () => {
+            await playerProfiles.deleteMany();
+        });
+
+        it('should return 400 when username parameter is not part of the request', async () => {
+
+            const response = await request(app)
+                                    .get("/api/player/box")
+                                    .query({});
+
+            expect(response.status).toBe(400);
+            expect(response.body).toMatchObject({});
+        })
+
+
+        it('should return GET error in response if any errors are thrown', async () => {
+
+            jest.spyOn(DatabaseService, 'findPlayerCaughtAnimals').mockRejectedValue();
+
+            const validUsername = "ValidName";
+
+            const response = await request(app)
+                                    .get("/api/player/box")
+                                    .query({
+                                        username: validUsername
+                                    });
+
+            expect(response.error.status).toBe(404);
+
+            expect(response.error.text).toEqual(expect.stringContaining(
+                "Cannot GET /api/player/box"
+            ));
+
+            jest.spyOn(DatabaseService, 'findPlayerCaughtAnimals').mockRestore();
+        })
+    })
+
+    describe('Valid Call', () => {
+
+        beforeAll(async () =>{
+
+            const badWikiQuery = 
+                'https://en.wikipedia.org/w/api.php?action=query&format=json' +
+                '&titles=' + 'NotAnAnimal' +
+                '&prop=pageimages|extracts&redirects=1&exintro&explaintext&pithumbsize=100';
+            
+            const badWikiResult =
+            {
+                "batchcomplete": "",
+                "query": {
+                    "pages": {
+                        "-1": {
+                            "ns": 0,
+                            "title": "NotAnAnimal",
+                            "missing": ""
+                        }
+                    }
+                }
+            }
+
+            const gatorWikiQuery =
+                'https://en.wikipedia.org/w/api.php?action=query&format=json' +
+                '&titles=' + 'Alligator%20mississippiensis' +
+                '&prop=pageimages|extracts&redirects=1&exintro&explaintext&pithumbsize=100';
+
+            const gatorWikiResult =
+            {
+                "batchcomplete": "",
+                "query": {
+                    "redirects": [
+                        {
+                            "from": "Alligator mississippiensis",
+                            "to": "American alligator"
+                        }
+                    ],
+                    "pages": {
+                        "484824": {
+                            "pageid": 484824,
+                            "ns": 0,
+                            "title": "American alligator",
+                            "thumbnail": {
+                                "source": "ThisWouldBeALinkToTheAlligatorImage/image.jpg",
+                                "width": 75,
+                                "height": 100
+                            },
+                            "pageimage": "American_Alligator.jpg",
+                            "extract": "The alligator is an alligator which is an alligator."
+                        }
+                    }
+                }
+            }
+
+            const gatorImageLink = "ThisWouldBeALinkToTheAlligatorImage/image.jpg";
+            const gatorImageBuffer = Buffer.from('ThisWouldBeTheGatorImageBinary');
+
+            axios.get.mockImplementation((queryUrl) => {
+                switch (queryUrl) {
+                    case badWikiQuery:
+                        return Promise.resolve({ data: badWikiResult });
+                    case gatorWikiQuery:
+                        return Promise.resolve({ data: gatorWikiResult });
+                    case gatorImageLink:
+                        return Promise.resolve({
+                            headers: {
+                                "content-type": "image/jpeg",
+                            },
+                            data: gatorImageBuffer
+                        })
+                }
+            })
+        });
+
+        beforeEach(async () => {
+            await playerProfiles.deleteMany();
+
+            const username = "RandomPlayer";
+            const email = "random@email.com";
+
+            const randomProfile = {
+                "user_name": username,
+                "user_email": email,
+                "collection": [
+                    {
+                        "common_name": "American alligator",
+                        "scientific_name": "Alligator mississippiensis",
+                        "count": 1
+                    }
+                ]
+            }
+
+            const username2 = "PlayerLackingAnimals"
+            const email2 = "random2@email.com";
+
+            const freshProfile = {
+                "user_name": username2,
+                "user_email": email2,
+                "collection": []
+            } 
+
+            await playerProfiles.insertOne(randomProfile);
+            await playerProfiles.insertOne(freshProfile);
+        });
+
+        it('should return 200 when parameter is valid', async () => {
+
+            const username = "RandomPlayer";
+
+            const response = await request(app)
+                                    .get("/api/player/box")
+                                    .query({
+                                        username: username
+                                    });
+
+            expect(response.status).toBe(200);
+        })
+
+
+        it('should return informative string when animals are not found', async () => {
+
+            const otherUsername = "PlayerLackingAnimal";
+
+            const response = await request(app)
+                                    .get("/api/player/box")
+                                    .query({
+                                        username: otherUsername
+                                    });
+
+            const foundMessage = response.body;
+
+            expect(foundMessage).toEqual(expect.objectContaining({
+                "your_animals": "No animals were found for the requested account."
+            }));
+        })
+
+        it('should return informative string when player doesn\'t exist', async () => {
+
+            const otherUsername = "OtherPlayer";
+
+            const response = await request(app)
+                                    .get("/api/player/box")
+                                    .query({
+                                        username: otherUsername
+                                    });
+
+            const foundMessage = response.body;
+
+            expect(foundMessage).toEqual(expect.objectContaining({
+                "your_animals": "No animals were found for the requested account."
+            }));
+        })
+
+        it('should return list of animals if they are found', async () => {
+
+            const username = "RandomPlayer";
+
+            const response = await request(app)
+                                    .get("/api/player/box")
+                                    .query({
+                                        username: username
+                                    });
+
+            const foundMessage = response.body;
+
+            expect(foundMessage).toEqual(expect.objectContaining({
+                "your_animals": [
+                    {
+                        "count": "1",
+                        "common_name": "American alligator",
+                        "scientific_name": "Alligator mississippiensis",
+                        "raw_image": "data:image/jpeg;base64,VGhpc1dvdWxkQmVUaGVHYXRvckltYWdlQmluYXJ5",
+                        "image_link": "ThisWouldBeALinkToTheAlligatorImage/image.jpg",
+                        "description": "The alligator is an alligator which is an alligator." 
+                    }
+                ]
+            }));
+        })
+    })
+})
+
 describe('PUT - /api/player/box', () => {
 
     describe('Error Checking', () => {
@@ -451,131 +802,6 @@ describe('PUT - /api/player/box', () => {
 
             expect(updateMessage).toEqual(expect.stringContaining(
                 "Player profile updated successfully"
-            ))
-        })
-    })
-})
-
-describe('DELETE - /api/player', () => {
-
-    describe('Error Checking', () => {
-
-        beforeEach(async () => {
-            await playerProfiles.deleteMany();
-        });
-
-        it('should return 400 when username parameter is not part of the request', async () => {
-
-            const response = await request(app)
-                                    .delete("/api/player")
-                                    .send({});
-
-            expect(response.status).toBe(400);
-            expect(response.body).toMatchObject({});
-        })
-
-        it('should return 422 when profile is not removed successfully', async () => {
-
-            const username = "NonExistantUser";
-
-            const response = await request(app)
-                                    .delete("/api/player")
-                                    .send({
-                                        username: username
-                                    });
-
-            expect(response.status).toBe(422);
-        })
-
-        it('should indicate the profile was not removed when remove is not successful', async () => {
-
-            const username = "NonExistantUser";
-
-            const response = await request(app)
-                                    .delete("/api/player")
-                                    .send({
-                                        username: username
-                                    });
-
-            const deleteMessage = response.body.remove_profile;
-
-            expect(deleteMessage).toEqual(expect.stringContaining(
-                "Profile not removed successfully"
-            ))
-        })
-
-        it('should return DELETE error in response if any errors are thrown', async () => {
-
-            jest.spyOn(DatabaseService, 'removePlayerProfile').mockRejectedValue();
-
-            const validName = "ValidUsername";
-
-            const response = await request(app)
-                                    .delete("/api/player")
-                                    .send({
-                                        username: validName
-                                    });
-
-            expect(response.error.status).toBe(404);
-
-            expect(response.error.text).toEqual(expect.stringContaining(
-                "Cannot DELETE /api/player"
-            ));
-
-            jest.spyOn(DatabaseService, 'removePlayerProfile').mockRestore();
-        })
-    })
-
-    describe('Valid Call', () => {
-
-        beforeEach(async () => {
-            await playerProfiles.deleteMany();
-
-            const username = "Knight";
-            const email = "random@email.com";
-
-            const newProfile = {
-                "user_name": username,
-                "user_email": email,
-                "collection": [
-                    {
-                        "common_name": "American alligator",
-                        "scientific_name": "Alligator mississippiensis",
-                        "count": 1
-                    }
-                ]
-            }
-
-            await playerProfiles.insertOne(newProfile);
-        });
-
-        it('should return 200 when parameters are valid and player profile is removed', async () => {
-
-            const username = "Knight";
-
-            const response = await request(app)
-                                    .delete("/api/player")
-                                    .send({
-                                        username: username
-                                    });
-
-            expect(response.status).toBe(200);
-        })
-
-        it('should indicate the profile was removed when remove is successful', async () => {
-
-            const username = "Knight";
-
-            const response = await request(app)
-                                    .delete("/api/player")
-                                    .send({
-                                        username: username
-                                    });
-
-            const deleteMessage = response.body.remove_profile;
-
-            expect(deleteMessage).toEqual(expect.stringContaining(
-                "Profile removed successfully"
             ))
         })
     })
